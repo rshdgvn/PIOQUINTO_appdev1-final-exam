30e644b2befbb999ce4c9ed3951b7949ca9cf9a7
rshdgvn <rasheedgavinesponga@gmail.com>
2025-11-29 14:54:56 +0800

    Fixed deployment configurations

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/PIOQUINTO_appdev1-final-exam/",
+  base: "/PIOQUINTO-appdev1-final-exam/",
   plugins: [react()],
 })
